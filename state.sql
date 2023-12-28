ea1427e4a07dac45ae5667b3adb7350f27dda323
Polyware Sdn Bhd <43948491+Suvindran94@users.noreply.github.com>
2023-12-28 09:39:12 +0800

    Create state.sql

---
+++ state.sql
@@ -0,0 +1,22 @@
+CREATE TABLE State (
+    state_id INT AUTO_INCREMENT PRIMARY KEY,
+    state_name VARCHAR(50) NOT NULL
+);
+
+INSERT INTO State (state_name) VALUES
+('Johor'),
+('Kedah'),
+('Kelantan'),
+('Melaka'),
+('Negeri Sembilan'),
+('Pahang'),
+('Perak'),
+('Perlis'),
+('Pulau Pinang'),
+('Sarawak'),
+('Selangor'),
+('Terengganu'),
+('Kuala Lumpur'),
+('Labuan'),
+('Sabah'),
+('Putrajaya');
